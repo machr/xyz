22d97812e74febe19f3beb8369d97dc798b52cc6
Mark Christiansen <hi@powellmark.com>
2023-08-29 15:20:22 +1000

    Change schema, show  1st link

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,16 +13,9 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
