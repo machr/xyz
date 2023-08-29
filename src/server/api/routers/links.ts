fa636d19881c081c0c33cd3223c25bfa8be1d59b
Mark Christiansen <hi@powellmark.com>
2023-08-29 17:35:19 +1000

    Add createLink functionality

--- src/server/api/routers/links.ts
+++ src/server/api/routers/links.ts
@@ -1,6 +1,7 @@
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "@/server/api/trpc";
 import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/server";
+import { z } from "zod";
 
 
 const filterUserforClient = (user: User) => {
@@ -28,5 +29,25 @@ export const linksRouter = createTRPCRouter({
             link,
             user: users.find((user) => user.id === link.userId),
         }))
-    })
+    }),
+
+    create: privateProcedure
+        .input(
+            z.object({
+                title: z.string(),
+                url: z.string(),
+                category: z.string()
+            }))
+        .mutation(async ({ ctx, input }) => {
+            const link = await ctx.prisma.link.create({
+                data: {
+                    userId: ctx.currentUser,
+                    title: input.title,
+                    url: input.url,
+                    category: input.category,
+                }
+            });
+
+            return link
+        })
 })
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -12,6 +12,7 @@ import superjson from "superjson";
 import { ZodError } from "zod";
 
 import { prisma } from "@/server/db";
+import { getAuth } from "@clerk/nextjs/server";
 
 /**
  * 1. CONTEXT
@@ -21,32 +22,20 @@ import { prisma } from "@/server/db";
  * These allow you to access things when processing a request, like the database, the session, etc.
  */
 
-type CreateContextOptions = Record<string, never>;
-
-/**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
- * it from here.
- *
- * Examples of things you may need it for:
- * - testing, so we don't have to mock Next.js' req/res
- * - tRPC's `createSSGHelpers`, where we don't have req/res
- *
- * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
- */
-const createInnerTRPCContext = (_opts: CreateContextOptions) => {
-  return {
-    prisma,
-  };
-};
-
 /**
  * This is the actual context you will use in your router. It will be used to process every request
  * that goes through your tRPC endpoint.
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = (_opts: CreateNextContextOptions) => {
-  return createInnerTRPCContext({});
+export const createTRPCContext = (opts: CreateNextContextOptions) => {
+  const { req } = opts
+  const session = getAuth(req)
+  const userId = session.userId
+  return {
+    prisma,
+    userId
+  };
 };
 
 /**
@@ -93,3 +82,18 @@ export const createTRPCRouter = t.router;
  * are logged in.
  */
 export const publicProcedure = t.procedure;
+
+const enforceUserIsAuthed = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.userId) {
+    throw new Error("Not authenticated")
+  }
+
+  return next({
+    ctx: {
+      currentUser: ctx.userId
+    }
+  })
+})
+
+
+export const privateProcedure = t.procedure.use(enforceUserIsAuthed)
